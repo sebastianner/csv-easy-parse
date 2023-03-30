54553f730af60e245713ab34ea852bd2898ff5b0
Sebastian <sebastianner23@gmail.com>
2023-03-30 18:17:38 -0500

    fromManyCvs finished, tsdocs added

--- src/services/csv-parser.ts
+++ src/services/csv-parser.ts
@@ -11,13 +11,14 @@ export function parse(
 		const csvToObject: CsvToObject = {};
 		const csvKey: string = Object.keys(content)[0];
 		const csv: string = Object.values(content)[0];
-		const rows: string[] | undefined = csv.trim().split("\n");
+		const rows: string[] | undefined = csv.trim().split("\r");
 		const headers: string[] | undefined = rows?.shift()?.split(separator);
 		csvToObject[csvKey] = {};
 		headers?.forEach((header: string, headerIndex: number) => {
 			csvToObject[csvKey][header] = [];
 			rows?.forEach((row) => {
-				const ObjectVal: string[] | number[] = row.split(separator);
+				const splitN = row.split("\n")[1];
+				const ObjectVal: string[] | number[] = splitN.split(separator);
 				csvToObject[csvKey][header].push(ObjectVal[headerIndex]);
 			});
 		});
