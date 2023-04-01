dde39d3a407fb0a497ddc0ce15dd19e9f3af1918
Sebastian <sebastianner23@gmail.com>
2023-04-01 15:40:10 -0500

    fix path resolver utility

--- src/utils/pathResolver.ts
+++ src/utils/pathResolver.ts
@@ -1,11 +1,5 @@
 import path from "path";
 
 export function pathResolver(filePath: string): string {
-	if (path.isAbsolute(filePath)) {
-		return filePath;
-	}
-
-	const dirname: string = new URL(".", import.meta.url).pathname;
-	const parentDir: string = path.dirname(dirname);
-	return path.join(parentDir, filePath);
+	return path.resolve(filePath);
 }
